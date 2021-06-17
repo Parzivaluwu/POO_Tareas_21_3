1d47866d4c60b92a7d09028a4260b0e0b12796da
Cesar <chesare149@hotmail.com>
2021-06-17 17:49:54 -0500

    Script Interface

---
+++ Assets/Scripts/Interfaces/InterfaceP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InterfaceP : MonoBehaviour
+{
+
+    public Text vidaTXT;
+    public Text puntuacionTXT;
+    Player playerData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerData=GetComponentInParent<Player>();
+        
+        StartCoroutine(AsignarDatos(0.35f));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator AsignarDatos(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        
+        vidaTXT.text=playerData.vida.ToString();
+        puntuacionTXT.text=playerData.puntuacion.ToString();
+        
+    }
+}
