d2b06c746ea9427430a9aa0dad9e748e21d7fff6
Cesar <chesare149@hotmail.com>
2021-06-17 17:05:03 -0500

    Constructor

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+     public int vida;
+   public float velocidadSalto;
+   public float velocidadMover;
+   private Transform posicion;
+   public string playerId;
+
+public int puntuacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
